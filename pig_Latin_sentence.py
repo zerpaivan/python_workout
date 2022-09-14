5e9e21a41038d9891212713de7bb56417b014e6c
zerpaian <zerpaivan@gmail.com>
2022-09-14 12:28:52 -0400

    add pig_latin_sentence script

---
+++ pig_Latin_sentence.py
@@ -0,0 +1,19 @@
+# script para covertir una frase en ingles a pig latin.
+# utilizando el ejercicio anterior:
+
+from pig_latin import PigLatin
+my_sentence = "my cat is called pepito"
+
+def pl_sentence(sentence):
+    words = sentence.split(" ")
+    print(words)
+    pig_words = []
+    for word in words:
+        pig_word = PigLatin(word)
+        pig_words.append(pig_word.word_to_pl())
+    
+    return " ".join(pig_words)
+
+
+if __name__ == "__main__":
+    print(pl_sentence(my_sentence))
